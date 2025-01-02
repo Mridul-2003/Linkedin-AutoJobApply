1d708e59d3da6ac516afad7e39c91aa3ffc905d0
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2025-01-02 21:55:06 +0530

    Update api.py

--- api.py
+++ api.py
@@ -53,7 +53,7 @@ def run_bot():
 
                 # Construct the command to execute runaibot.py
                 
-                command = ['python', '/Users/useradmin/Desktop/AutoJobsApply/Linkedin/runAiBot.py', json.dumps(user_config)]
+                command = ['python', 'runAiBot.py', json.dumps(user_config)]
 
                         # Start the runaibot.py script in the background.  Consider using a process pool for better management.
                 subprocess.Popen(command)
