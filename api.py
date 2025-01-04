1e8a785bf7cd4402c23f8351bd672eb8cca8773d
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2025-01-04 23:19:41 +0530

    Update api.py

--- api.py
+++ api.py
@@ -67,4 +67,4 @@ def run_bot():
         return jsonify({"error": f"Failed to start the bot: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True, port=56581)
+    app.run(host="0.0.0.0",debug=True, port=8000)
