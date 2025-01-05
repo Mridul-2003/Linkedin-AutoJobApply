550cbee5f76741710e9e59f87ae7decac9263c26
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2025-01-05 17:34:05 +0530

    Update open_chrome.py

--- modules/open_chrome.py
+++ modules/open_chrome.py
@@ -2,12 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from pyvirtualdisplay import Display
     #from selenium.webdriver.chrome.service import Service as ChromeService # remove webdriver_manager
     #from webdriver_manager.chrome import ChromeDriverManager # remove webdriver_manager
 
 try:
+    display = Display(visible=1, size=(1920, 1080))  # Adjusted for headless operation
+    display.start()
     options = Options()
-    options.add_argument("--headless")
     options.add_argument("--disable-gpu")  # Disable GPU acceleration
     options.add_argument("--no-sandbox")  # Required in some environments
     options.add_argument("--disable-dev-shm-usage")# Avoids out-of-memory issues# Set the correct path to chrome
