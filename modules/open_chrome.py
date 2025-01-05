4feb71d509007c12bc158ea6b589669803b91e2d
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2025-01-05 17:46:06 +0530

    Update open_chrome.py

--- modules/open_chrome.py
+++ modules/open_chrome.py
@@ -13,8 +13,9 @@ try:
     options.add_argument("--disable-gpu")  # Disable GPU acceleration
     options.add_argument("--no-sandbox")  # Required in some environments
     options.add_argument("--disable-dev-shm-usage")# Avoids out-of-memory issues# Set the correct path to chrome
-    driver = webdriver.Chrome(options=options)  # Set correct path to chromedriver
-    print("Chrome driver instantiated successfully in headless mode.")
+    driver = webdriver.Chrome(service=ChromeDriverManager().install(), options=options)
+    print("Chrome driver instantiated successfully in headless mode.")  # Set correct path to chromedriver
+  
     driver.quit()
 except Exception as e:
     print(f"Error: {e}")
